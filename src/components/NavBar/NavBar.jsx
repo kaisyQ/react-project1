c2dd74b3d0dc7d46f32ac99c75d5f364816dd617
Kaisy <rutyo8@mail.ru>
2022-09-04 18:51:49 +1100

    nav-updt

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,17 +1,17 @@
 import React from "react"
 import { NavLink } from "react-router-dom"
-import css from './NavBar.module.css'
+import css from './NavBar.module.scss'
 
 const NavBar = () => {
-    return (
+    return <>
         <nav className={css.nav}>
-            <NavLink className={css.item} to="/Profile">Profile</NavLink>
-            <NavLink className={css.item} to="/Users">Users</NavLink>
-            <NavLink className={css.item} to="/Dialogs">Messages</NavLink>
-            <NavLink className={css.item} to="/News">News</NavLink>
-            <NavLink className={css.item} to="/Settings">Settings</NavLink>
+            <div className={css.navItem}><NavLink to="/Profile">Profile</NavLink></div>
+            <div className={css.navItem}><NavLink to="/Users">Users</NavLink></div>
+            <div className={css.navItem}><NavLink to="/Dialogs">Messages</NavLink></div>
+            <div className={css.navItem}><NavLink to="/News">News</NavLink></div>
+            <div className={css.navItem}><NavLink to="/Settings">Settings</NavLink></div>
         </nav>
-    )
+    </>
 }
 
 export default NavBar
